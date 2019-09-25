971dd89efc83547323338c0e7b02d184a63b3937
Nikita <nikitamasalkov.tang@gmail.com>
2019-09-25 17:00:38 +0300

    added bootstrap with webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,7 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import '../stylesheets/application'
+import './bootstrap_custom.js'
+
 console.log('Hello World from Webpacker')
