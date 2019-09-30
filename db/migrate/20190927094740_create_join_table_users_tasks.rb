f8d17c01b240f9f9eb2d60221df5f77deebcdc8e
Nikita <nikitamasalkov.tang@gmail.com>
2019-09-30 14:46:24 +0300

    created joining table between users and tasks to avoid doubles

--- db/migrate/20190927094740_create_join_table_users_tasks.rb
+++ db/migrate/20190927094740_create_join_table_users_tasks.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableUsersTasks < ActiveRecord::Migration[5.2]
   def change
     create_join_table :users, :tasks do |t|
-      # t.index [:user_id, :task_id]
-      # t.index [:task_id, :user_id]
+      t.index [:user_id, :task_id], unique: true
+      t.index [:task_id, :user_id], unique: true
     end
   end
 end
