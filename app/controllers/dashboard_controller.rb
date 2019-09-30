54321eccfcb83323c2c2e161a71187e51c223085
Nikita <nikitamasalkov.tang@gmail.com>
2019-09-30 17:42:44 +0300

    made initial pundit user scope

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
     tasks = Task.all.includes(:users)
+
     tasks_waiting_list = tasks.select(&:waiting_list?)
     tasks_in_progress = tasks.select(&:in_progress?)
     tasks_done = tasks.select(&:done?)
