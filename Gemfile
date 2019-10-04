9237a13dde94cf76480a37fac2eea1becd2518bc
Nikita <nikitamasalkov.tang@gmail.com>
2019-10-04 14:23:34 +0300

    review refactoring

--- Gemfile
+++ Gemfile
@@ -14,7 +14,6 @@ gem 'sass-rails', '~> 5.0'
 # Custom gems
 gem 'webpacker'
 gem 'devise'
-gem 'pry'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
@@ -52,6 +51,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry'
 end
 
 
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,16 @@
 class DashboardController < ApplicationController
   def index
     tasks = Task.all.includes(:users)
-    tasks_waiting_list = tasks.select { |task| task.status == 'waiting_list' }
-    tasks_in_progress = tasks.select { |task| task.status == 'in_progress' }
-    tasks_done = tasks.select { |task| task.status == 'done' }
+    tasks_waiting_list = tasks.select(&:waiting_list?)
+    tasks_in_progress = tasks.select(&:in_progress?)
+    tasks_done = tasks.select(&:done?)
     users = User.all
-    regular_users = users.select { |user| user.role == 'user' }
-    admin_users = users.select { |user| user.role == 'admin' }
-    render locals: { tasks_done: tasks_done, tasks_waiting_list: tasks_waiting_list, tasks_in_progress: tasks_in_progress, tasks: tasks, regular_users: regular_users, admin_users: admin_users }
+    regular_users = users.select(&:user?)
+    admin_users = users.select(&:admin?)
+    render locals: { tasks_done: tasks_done,
+                     tasks_waiting_list: tasks_waiting_list,
+                     tasks_in_progress: tasks_in_progress,
+                     tasks: tasks, regular_users: regular_users,
+                     admin_users: admin_users }
   end
 end
