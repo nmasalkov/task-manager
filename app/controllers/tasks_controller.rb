a514d9dc84df509c576e72061113f3f3c521ca6a
NikitaMasalkov <42782407+NikitaMasalkov@users.noreply.github.com>
2019-12-23 12:01:26 +0300

    Switch to form object (#9)
    * made initial pundit user scope
    
    * make user policy and scope for it
    
    * add initial task creation form
    
    * finish task create, show and edit features
    
    * prevent tasks from being edited or created without user
    
    * make task policy, devided assignees and creators
    
    * make major refactoring to catch up with master
    
    * review refactoring
    
    * switch from joining model to two separate relations between users and tasks
    
    * made initial structure for both virtus and dry forms
    
    * create initial functional form object
    
    * refactored form object
    
    * refactored task_form
    
    * revive branch

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,16 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!, except: :show
+  before_action :find_task, only: %i[edit update show]
 
   def new
-    task = Task.new
-    users = User.all
-    statuses = Task.statuses
-    render locals: { task: task,
-                     users: users,
-                     statuses: statuses }
+    @task_form = TaskForm.new(current_user)
+    @users = User.all
+    @statuses = Task.statuses
   end
 
   def create
-    task = Task.new(task_params)
-    task.set_creator(current_user)
-    if task.save
+    @task_form = TaskForm.new(current_user, task_params)
+    if @task_form.save
       redirect_to root_path
     else
       redirect_to new_task_path
@@ -21,42 +18,38 @@ class TasksController < ApplicationController
   end
 
   def edit
-    task = find_task
-    users = User.all
-    statuses = Task.statuses
-    render locals: { task: task,
-                     users: users,
-                     statuses: statuses }
+    @task_form = TaskForm.new(current_user, {}, @task)
+    @users = User.all
+    @statuses = Task.statuses
   end
 
   def update
-    task = find_task
-
-    if task.update(task_params)
-      redirect_to task_path(task.id)
+    @task_form = TaskForm.new(current_user, task_params, @task)
+    if @task_form.save
+      redirect_to task_path(@task)
     else
-      redirect_to edit_task_path(task.id)
+      redirect_to edit_task_path(@task)
     end
   end
 
   def show
-    task = find_task
-    users = task.users
-    render locals: { task: task, users: users }
+    @users = @task.users
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title,
-                                 :description,
-                                 :status,
-                                 :start_date,
-                                 :end_date,
-                                 user_ids: [])
+    task_params = params.require(:task).permit(:title,
+                                                :description,
+                                                :status,
+                                                :start_date,
+                                                :end_date,
+                                                user_ids: [])
+
+    NormalizeDatesService.new(task_params).normalize_date_attr
   end
 
   def find_task
-    Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 end
